ad9e686f11071afb78aff9722832655562e1707b
Paulo Santos <sungonku@gmail.com>
2016-03-20 18:49:45 +0000

    able to download file from url and parse it

--- server/startup.js
+++ server/startup.js
@@ -1,15 +1,21 @@
+/* global FS, Files, Excel */
 
 var url = "http://www.dripinvesting.org/tools/U.S.DividendChampions.xls";
-var response = HTTP.call( 'GET', url, {} );
 
-var fs = Npm.require('fs');
+var objFile = Files.findOne();
+if (!objFile) {
+    objFile = Files.insert(url);
+} else if (objFile.uploadedAt.getTime() < new Date() - 30*24*60*60*100 || Files.find().count() > 1) {
+    while (Files.find().count() > 0) {
+        Files.remove({_id: Files.findOne()._id});
+    }
+    objFile = Files.insert(url);
+}
+
 var path = Npm.require('path');
 var basepath = path.resolve('.').split('.meteor')[0];
 
 var excel = new Excel('xls');
-
-var workbook = excel.readFile( basepath+'public/c.xls');
-
+var workbook = excel.readFile(basepath + "uploads/"+ Files.findOne().copies.files.key);
 var yourSheetsName = workbook.SheetNames;
-
 console.log(yourSheetsName);

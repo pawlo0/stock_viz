461340c0fd2dc8a73472e07272cea2b7d5800629
Paulo Santos <sungonku@gmail.com>
2016-03-23 15:36:27 +0000

    fixed session set when first load of page

--- client/stock_viz.js
+++ client/stock_viz.js
@@ -11,17 +11,37 @@ Template.stock_viz_controls.helpers({
     
     // returns an array of the names of all features of the requested type
     "get_fundamental_names": function(){
+        
         // pull an example from the database
         // - we'll use this to find the names of all features
         var stock = Stocks.findOne();
+        if (Session.get("study") == undefined){
+            Session.set("study", Object.keys(stock.fundamentals)[0]);
+        }
+        
         if (stock){
-            var feat_list = [];
+            var feat_list = [], selected = "";
             for ( var f in stock.fundamentals) {
-                feat_list.push({name: f});
+                if (Session.get("study") == f) {
+                    selected = "selected";
+                } else {
+                    selected = "";
+                }
+                feat_list.push({name: f, isSelected: selected});
             }
         }
         return feat_list;
     },
+    "get_groups": function(){
+        if (Session.get("group") == undefined){
+            Session.set("group", "Sector");
+        }
+        var groups = [
+            {group: "Sector"},
+            {group: "Industry"}
+            ];
+        return groups;
+    }
 });
 
 
@@ -31,10 +51,7 @@ Template.stock_viz_controls.helpers({
 
 Template.stock_fundamentals_list.helpers({
     "get_fundamental_values":function(){
-        if (Session.get("study") == undefined){
-            Session.set("study", $(".js-select-single-fundamental :selected").val());
-        }
-        
+
         // Had to be careful not to pass values that are not numbers because sometimes we get values like "n/a"
         // If so, it would mess the data, so I decided to filter the data.
         // The way to do that was through filtering the mongo query to only retrieve double numbers (same as $type:1)
@@ -72,6 +89,10 @@ Template.stock_viz_controls.events({
     // option in the drop down list
     "change .js-select-single-fundamental":function(event){
         Session.set("study", event.target.value);
+        event.preventDefault();
+    },
+    "change .js-select-grouping":function(event){
+        Session.set("group", event.target.value);
         event.preventDefault();
     },
     // event handler for when the user clicks on the 

67ab47745543b796082bd6b9956d1cb814a1d804
Paulo Santos <sungonku@gmail.com>
2016-03-31 10:50:36 +0000

    only run excel parse if date is 30 days old

--- client/layout.js
+++ client/layout.js
@@ -16,35 +16,36 @@ Template.layout.onRendered(function(){
 Template.layout.helpers({
     "checkData": function (e) {
         if (Files.findOne()){
-            var url = Files.findOne().url();
-            var oReq = new XMLHttpRequest();
-            oReq.open("GET", url, true);
-            oReq.responseType = "arraybuffer";
-            
-            oReq.onload = function(e) {
-              var arraybuffer = oReq.response;
-            
-              /* convert data to binary string */
-              var data = new Uint8Array(arraybuffer);
-              var arr = new Array();
-              for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-              var bstr = arr.join("");
-            
-              /* Call XLSX */
-              var workbook = XLSX.read(bstr, {type:"binary"});
-              // I know before hand that we need the 4th sheet.
-              var workSheet = workbook.Sheets[workbook.SheetNames[3]];
-              Meteor.call("insertDB", workSheet, function(error, result){
-                    if(error){
-                        console.log(error);
-                    } else {
-                        console.log(result);
-                    }
-                });
-            };
-            oReq.send();
+            if (!Stocks.findOne() || new Date() - Stocks.findOne().updatedAt > 30*24*60*60*1000) {
+                var url = Files.findOne().url();
+                var oReq = new XMLHttpRequest();
+                oReq.open("GET", url, true);
+                oReq.responseType = "arraybuffer";
+                
+                oReq.onload = function(e) {
+                  var arraybuffer = oReq.response;
+                
+                  /* convert data to binary string */
+                  var data = new Uint8Array(arraybuffer);
+                  var arr = new Array();
+                  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+                  var bstr = arr.join("");
+                
+                  /* Call XLSX */
+                  var workbook = XLSX.read(bstr, {type:"binary"});
+                  // I know before hand that we need the 4th sheet.
+                  var workSheet = workbook.Sheets[workbook.SheetNames[3]];
+                  Meteor.call("insertDB", workSheet, function(error, result){
+                        if(error){
+                            console.log(error);
+                        } else {
+                            console.log(result);
+                        }
+                    });
+                };
+                oReq.send();
+            }
         }
-        
         var screen = $(window).height();
         if ($('#network').height() < screen) {
             $('#network').height(screen);
